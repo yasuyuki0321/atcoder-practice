cfb53141ace6ebcff731562d96f3458fae8de03d
Yasuyuki Kikuchi <yasuyuki0321@gmail.com>
2022-10-29 08:55:44 +0900

    feat: abc084の追加

--- abc164_b/main.go
+++ abc164_b/main.go
@@ -1,1 +1,24 @@
+package main
 
+import "fmt"
+
+func main() {
+	var a, b, c, d int
+	fmt.Scan(&a, &b, &c, &d)
+
+	for i := 1; ; i++ {
+		if i%2 == 1 {
+			c -= b
+			if c <= 0 {
+				fmt.Println("Yes")
+				break
+			}
+		} else {
+			a -= d
+			if a <= 0 {
+				fmt.Println("No")
+				break
+			}
+		}
+	}
+}
